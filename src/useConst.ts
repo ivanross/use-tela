422042a8b55869c00f800d4665952076968f402d
Ivan Rossi <ivanross.me@gmail.com>
2021-12-27 18:22:44 +0000

    fix: lint error

--- src/useConst.ts
+++ src/useConst.ts
@@ -1,10 +1,10 @@
-import { useRef } from "react";
+import { useRef } from 'react';
 
 const INIT = {};
 export function useConst<T>(value: T | (() => T)) {
   const ref = useRef(INIT as T);
   if (ref.current === INIT) {
-    ref.current = typeof value === "function" ? (value as () => T)() : value;
+    ref.current = typeof value === 'function' ? (value as () => T)() : value;
   }
 
   return ref.current;
