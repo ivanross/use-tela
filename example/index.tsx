54560852f6a4a813348eb907463898f13a87f155
Ivan Rossi <ivanross.me@gmail.com>
2021-12-28 00:12:45 +0000

    docs: update example

--- example/index.tsx
+++ example/index.tsx
@@ -16,6 +16,48 @@ const simplex = new SimplexNoise();
 const stats = new Stats();
 
 document.body.appendChild(stats.dom);
+
+class Dot {
+  x: number;
+  y: number;
+
+  constructor(width: number, height: number) {
+    this.random(width, height);
+  }
+
+  random(width: number, height: number) {
+    this.x = Math.random() * width;
+    this.y = Math.random() * height;
+  }
+  update(time: number) {
+    const { x, y } = this;
+
+    const t = time * 0.4;
+    const amp = 2;
+    const sp = 0.005;
+    this.x += amp * simplex.noise3D(x * sp, y * sp, t);
+    this.y += amp * simplex.noise3D(x * sp, y * sp, t + 1);
+  }
+}
+
+class Model {
+  dots: Dot[];
+  i = 0;
+
+  constructor(width: number, height: number) {
+    this.dots = _.range(10000).map(() => new Dot(width, height));
+  }
+
+  update(width: number, height: number, time: number) {
+    _.times(150).forEach(() => {
+      this.i = (this.i + 1) % this.dots.length;
+      this.dots[this.i].random(width, height);
+    });
+
+    this.dots.forEach(dot => dot.update(time));
+  }
+}
+
 function App() {
   const { fill, showConsole } = useControls({
     fill: 'teal',
@@ -23,26 +65,7 @@ function App() {
   });
 
   const { width, height } = useWindowSize();
-  const model = useConst(() => {
-    class Dot {
-      x = Math.random() * width;
-      y = Math.random() * height;
-
-      update(time: number) {
-        const { x, y } = this;
-
-        const t = time * 0.008;
-        const amp = 2;
-        const sp = 0.005;
-        this.x += simplex.noise3D(x * sp, y * sp, time * t) * amp;
-        this.y += simplex.noise3D(x * sp, y * sp, 10 + time * t) * amp;
-      }
-    }
-
-    return {
-      dots: _.range(5000).map(() => new Dot()),
-    };
-  });
+  const model = useConst(() => new Model(width, height));
 
   const ref = useCanvas({
     width,
@@ -52,12 +75,14 @@ function App() {
     draw: e => {
       stats.begin();
 
-      model.dots.forEach(dot => dot.update(e.time));
+      model.update(e.width, e.height, e.time);
 
       C.draw(e.canvas, [
         C.rect(0, 0, e.width, e.height, { fill: 'black', opacity: 0.08 }),
 
-        ...model.dots.map(dot => C.circle(dot.x, dot.y, 2, { fill })),
+        ...model.dots.map(dot =>
+          C.circle(dot.x, dot.y, 1, { fill, opacity: 0.3 })
+        ),
       ]);
 
       stats.end();
